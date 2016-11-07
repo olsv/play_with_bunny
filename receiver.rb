37053b440d57147fc2097713711c288e5586054a
Oleg Sviridovitch <oleg.sviridovitch@rightscale.com>
2016-11-07 13:08:35 +0300

    dockerize

--- receiver.rb
+++ receiver.rb
@@ -1,24 +1,26 @@
 #!/usr/bin/env ruby
-#encoding: utf-8
+# encoding: utf-8
 
 require 'bunny'
+require 'yaml'
+RACK_ENV ||= :development
 
-conn = Bunny.new
+conn = Bunny.new(YAML.load_file('config/rabbitmq.yml')[RACK_ENV.to_sym])
 conn.start
 
 ch = conn.create_channel
-x = ch.fanout("logs")
-q  = ch.queue('', exclusive: true)
+x = ch.fanout('logs')
+q = ch.queue('', exclusive: true)
 
 q.bind(x)
 
 puts " [*] Waiting for messages in #{q.name}. To exit press CTRL+C"
 begin
-  q.subscribe(manual_ack: true, block: true) do |delivery_info, properties, body|
+  q.subscribe(manual_ack: true, block: true) do |delivery_info, _props, body|
     puts " [x] Received #{body}"
 
     sleep body.count('.').to_i
-    puts " [x] Done"
+    puts ' [x] Done'
     ch.ack(delivery_info.delivery_tag)
   end
 rescue Interrupt => _
--- sender.rb
+++ sender.rb
@@ -1,15 +1,17 @@
 #!/usr/bin/env ruby
-#encoding: utf-8
+# encoding: utf-8
 
 require 'bunny'
+require 'yaml'
+RACK_ENV ||= :development
 
-conn = Bunny.new
+conn = Bunny.new(YAML.load_file('config/rabbitmq.yml')[RACK_ENV.to_sym])
 conn.start
 
 ch = conn.create_channel
-x = ch.fanout("logs")
+x = ch.fanout('logs')
 
-msg = ARGV.empty? ? "Hello world!" : ARGV.join(" ")
+msg = ARGV.empty? ? 'Hello world!' : ARGV.join(' ')
 
 x.publish(msg)
 puts " [x] Sent '#{msg}'"
